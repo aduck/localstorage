b40f680b990786712d9906980a1bbb178c9bd8c1
higher <781957583@qq.com>
2017-04-24 18:10:55 +0800

    fix code

--- router/files.js
+++ router/files.js
@@ -3,10 +3,12 @@ const router=express.Router()
 const fs=require('fs')
 const path=require('path')
 const config=require('../config')
+const url=require('url')
 
 router
   .use('/',(req,res,next)=>{
     let reqpath=req.path.slice(1)
+    reqpath=decodeURI(reqpath)
     let realpath=path.resolve(config.path,reqpath)
     fs.lstat(realpath,(err,stats)=>{
       if(err) return next(err)
